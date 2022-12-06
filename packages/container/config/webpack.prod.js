f287fc64a3af0e2e71640ea5f87a06182e2ee4db
Shardendu Kantha <shardenk@amazon.com>
2022-12-06 15:06:18 -0800

    checking production domain url issue

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -6,20 +6,20 @@ const packageJson = require('../package.json');
 const domain = process.env.PRODUCTION_DOMAIN; // should be  a string value of domain and needs to be set up at the time of CI/CD pipeline setup
 
 const prodConfig = {
-    mode: 'production',
-    output: {
-        filename: '[name].[contenthash].js',
-        publicPath: '/container/latest/'
-    },
-    plugins: [
-        new ModuleFederationPlugin({
-            name: 'container',
-            remotes: {
-                marketing: `marketing@h${domain}/marketing/latest/remoteEntry.js` // assuming remotrEntry will be localted inside marketing/latest folder
-            },
-            shared: packageJson.dependencies, // all the dev dependencies will be shared
-        }),
-    ]
-}
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js',
+    publicPath: '/container/latest/',
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'container',
+      remotes: {
+        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`, // assuming remotrEntry will be located inside marketing/latest folder
+      },
+      shared: packageJson.dependencies, // all the dev dependencies will be shared
+    }),
+  ],
+};
 
 module.exports = merge(commonConfig, prodConfig);
